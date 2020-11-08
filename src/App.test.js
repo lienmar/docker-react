1b8287137c271ae6103f0cc084b911b814fae6dd
Martin Lien <martinlien@pm.me>
2020-11-08 15:32:50 +0100

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
